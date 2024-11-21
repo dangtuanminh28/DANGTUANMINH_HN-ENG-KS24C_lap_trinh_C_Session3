ccf4bf339de2ffd063caad6dacf7f34faeae42a7
dangtuanminh28 <dangmanhcuong27011971@gmail.com>
2024-11-21 23:41:55 +0700

    Upload bài Lập trình C cơ bản Session3-4

---
+++ Session3-4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+// khai bao bien
+   float toan;
+   float van;
+   float anh;
+   float average;
+   float sum;   
+   printf("nhap so thuc diem toan: ");
+   scanf("%f", &toan);
+   printf("nhap so thuc diem van: ");
+   scanf("%f", &van);
+   printf("nhap so thuc diem anh: ");
+   scanf("%f", &anh);
+// cong thuc tinh tong diem
+   sum = toan + van + anh;
+   printf("vay tong diem la: %.2f\n", sum);
+// cong thuc tinh diem trung binh
+   average = (toan + van + anh)/3; 
+   printf("vay diem trung binh la: %.2f", average);
+	return 0;
+}	
